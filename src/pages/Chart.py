17942ab7362434832e99cb2f046e19c16a1a7138
juan miguel <miguel.rojas@jala.university>
2025-05-30 00:06:05 -0500

    modified name

--- src/pages/Chart.py
+++ src/pages/Chart.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from urllib.error import URLError
+import os
 
 st.set_page_config(page_title="DataFrame Demo", page_icon="./assets/Logo-removebg.png")
 
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
-st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
-)
+st.markdown("# Índice de Criminalidad")
+
+st.subheader("Selecciona un distrito")
 
 
 @st.cache_data
